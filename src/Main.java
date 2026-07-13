e6ff7349861d9f45ae5a9a13aec2d6c0073adf1d
johns3pn <johns3pn@mail.uc.edu>
2026-07-13 19:49:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,129 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+
+        int[] dataPoints = new int[100];
+        for (int index = 0; index < dataPoints.length; index++)
+        {
+            dataPoints[index] = rnd.nextInt(100) + 1;
+        }
+        System.out.println("The values in the array are:");
+
+        for (int index = 0; index < dataPoints.length; index++)
+        {
+            System.out.print(dataPoints[index] + " | ");
+        }
+
+        System.out.println();
+        int sum = 0;
+
+        for (int index = 0; index < dataPoints.length; index++)
+        {
+            sum = sum + dataPoints[index];
+        }
+
+        double average = (double) sum / dataPoints.length;
+
+        System.out.println();
+        System.out.println("The sum of the values is: " + sum);
+        System.out.println("The average of the values is: " + average);
+
+        int searchValue = SafeInput.getRangedInt(
+                in,
+                "Enter a number to search for",
+                1,
+                100
+        );
+        int count = 0;
+
+        for (int index = 0; index < dataPoints.length; index++)
+        {
+            if (dataPoints[index] == searchValue)
+            {
+                count++;
+            }
+        }
+
+        System.out.println(
+                "The number " + searchValue +
+                        " was found " + count + " time(s) in the array."
+        );
+        int secondSearchValue = SafeInput.getRangedInt(
+                in,
+                "Enter another number to search for",
+                1,
+                100
+        );
+
+        boolean found = false;
+        int foundIndex = -1;
+
+        for (int index = 0; index < dataPoints.length; index++)
+        {
+            if (dataPoints[index] == secondSearchValue)
+            {
+                found = true;
+                foundIndex = index;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            System.out.println(
+                    "The number " + secondSearchValue +
+                            " was first found at array index " + foundIndex + "."
+            );
+        }
+        else
+        {
+            System.out.println(
+                    "The number " + secondSearchValue +
+                            " was not found in the array."
+            );
+            int minimum = dataPoints[0];
+            int maximum = dataPoints[0];
+
+            for (int index = 1; index < dataPoints.length; index++)
+            {
+                if (dataPoints[index] < minimum)
+                {
+                    minimum = dataPoints[index];
+                }
+
+                if (dataPoints[index] > maximum)
+                {
+                    maximum = dataPoints[index];
+                }
+            }
+
+            System.out.println("The minimum value in the array is: " + minimum);
+            System.out.println("The maximum value in the array is: " + maximum);
+            double[] doubleDataPoints = new double[dataPoints.length];
+
+            for (int index = 0; index < dataPoints.length; index++)
+            {
+                doubleDataPoints[index] = dataPoints[index];
+            }
+
+            System.out.println("Average of dataPoints is: " + getAverage(doubleDataPoints));
+        }
+    }
+    public static double getAverage(double[] values)
+    {
+        double total = 0;
+
+        for (int index = 0; index < values.length; index++)
+        {
+            total = total + values[index];
+        }
+
+        return total / values.length;
+    }
+}
---
+++ src/SafeInput.java
@@ -0,0 +1,214 @@
+import java.util.Scanner;
+
+public class SafeInput {
+
+    /**
+     *
+     * @param pipe Scanner object
+     * @param prompt prompt for user
+     * @return non-empty String
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = "";
+
+        do
+        {
+            System.out.print("\n" + prompt + ": ");
+            retString = pipe.nextLine();
+
+        } while(retString.length() == 0);
+
+        return retString;
+    }
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int value;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + ": ");
+
+            if(pipe.hasNextInt())
+            {
+                value = pipe.nextInt();
+                pipe.nextLine();
+                return value;
+            }
+            else
+            {
+                System.out.println("Invalid integer. Try again.");
+                pipe.nextLine();
+            }
+        }
+    }
+    public static double getDouble(Scanner pipe, String prompt)
+    {
+        double value;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + ": ");
+
+            if(pipe.hasNextDouble())
+            {
+                value = pipe.nextDouble();
+                pipe.nextLine();
+                return value;
+            }
+            else
+            {
+                System.out.println("Invalid number. Try again.");
+                pipe.nextLine();
+            }
+        }
+    }
+
+    public static int getRangedInt(Scanner pipe, String prompt, int low, int high)
+    {
+        int value;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+
+            if(pipe.hasNextInt())
+            {
+                value = pipe.nextInt();
+                pipe.nextLine();
+
+                if(value >= low && value <= high)
+                {
+                    return value;
+                }
+                else
+                {
+                    System.out.println("Input must be between " + low + " and " + high + ".");
+                }
+            }
+            else
+            {
+                System.out.println("Invalid integer. Try again.");
+                pipe.nextLine();
+            }
+        }
+    }
+
+    public static double getRangedDouble(Scanner pipe, String prompt, double low, double high)
+    {
+        double value;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + " [" + low + " - " + high + "]: ");
+
+            if(pipe.hasNextDouble())
+            {
+                value = pipe.nextDouble();
+                pipe.nextLine();
+
+                if(value >= low && value <= high)
+                {
+                    return value;
+                }
+                else
+                {
+                    System.out.println("Input must be between " + low + " and " + high + ".");
+                }
+            }
+            else
+            {
+                System.out.println("Invalid number. Try again.");
+                pipe.nextLine();
+            }
+        }
+    }
+
+    public static boolean getYNConfirm(Scanner pipe, String prompt)
+    {
+        String response;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + " [Y/N]: ");
+
+            response = pipe.nextLine();
+
+            if(response.equalsIgnoreCase("Y"))
+            {
+                return true;
+            }
+            else if(response.equalsIgnoreCase("N"))
+            {
+                return false;
+            }
+            else
+            {
+                System.out.println("Please enter Y or N.");
+            }
+        }
+    }
+
+    public static String getRegExString(Scanner pipe, String prompt, String regEx)
+    {
+        String response;
+
+        while(true)
+        {
+            System.out.print("\n" + prompt + ": ");
+
+            response = pipe.nextLine();
+
+            if(response.matches(regEx))
+            {
+                return response;
+            }
+            else
+            {
+                System.out.println("Invalid format. Try again.");
+            }
+        }
+    }
+
+    public static void prettyHeader(String msg)
+    {
+        int width = 60;
+        int insideWidth = width - 6; // 3 stars on each side
+
+        int spaces = (insideWidth - msg.length()) / 2;
+        int remainingSpaces = insideWidth - msg.length() - spaces;
+
+        for(int i = 0; i < width; i++)
+        {
+            System.out.print("*");
+        }
+
+        System.out.println();
+
+        System.out.print("***");
+
+        for(int i = 0; i < spaces; i++)
+        {
+            System.out.print(" ");
+        }
+
+        System.out.print(msg);
+
+        for(int i = 0; i < remainingSpaces; i++)
+        {
+            System.out.print(" ");
+        }
+
+        System.out.println("***");
+
+        for(int i = 0; i < width; i++)
+        {
+            System.out.print("*");
+        }
+
+        System.out.println();
+    }
+
+}
+
+
